bb360938dc4be779e0e8c8eec2cc6f3476ee9335
chenling <13439070649@163.com>
2019-02-22 13:43:25 +0800

    修改

--- src/main/java/com/zcsj/wx/controller/WxController.java
+++ src/main/java/com/zcsj/wx/controller/WxController.java
@@ -1,8 +1,13 @@
 package com.zcsj.wx.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.zcsj.wx.constant.CommonConstants;
 import com.zcsj.wx.constant.WxConstant;
 import com.zcsj.wx.vo.BaseResultVO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 import java.io.BufferedReader;
