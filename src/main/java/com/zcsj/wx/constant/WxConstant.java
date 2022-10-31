8ae799a8b46af1dcae159d289073aa3cf2ce1fbe
chenling1995 <39896747+chenling1995@users.noreply.github.com>
2022-10-31 16:39:35 +0800

    Update WxConstant.java

--- src/main/java/com/zcsj/wx/constant/WxConstant.java
+++ src/main/java/com/zcsj/wx/constant/WxConstant.java
@@ -3,9 +3,9 @@ package com.zcsj.wx.constant;
 public class WxConstant {
 	
 
-	public static final String AppID = "wx164e1c974fd00643";
+	public static final String AppID = "xxxxxx";
 
-	public static final String AppSecret="36de54dc134b489f46243d54bf08987c";
+	public static final String AppSecret="xxxxxxx";
     //获取AccessToken的url
     public static final  String GET_ACCESS_TOKEN_URL = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + AppID + "&corpsecret=" + AppSecret;
 
